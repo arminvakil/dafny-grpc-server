bce3f84418ba48231c088ed2e6e7aee40f59fa29
Armin Vakil <arminvakil@gmail.com>
2023-09-24 19:14:13 -0400

    add processor count as input

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <iostream>
 #include <pthread.h>
+#include <thread>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -27,14 +28,15 @@ DafnyVerifierServiceImpl *verifier_service;
 
 void usage(char **argv)
 {
-    std::cout << "Usage: " << argv[0] << " -d Dafny_EXE_PATH [-i IP:PORT]\n";
+    std::cout << "Usage: " << argv[0] << " -d DAFNY_EXE_PATH -j NUM_PHYSICAL_CORES [-i IP:PORT]\n";
 }
 
 int main(int argc, char **argv)
 {
     char opt;
+    int processor_count = -1;
 
-    while ((opt = getopt(argc, argv, "hvi:d:")) != -1)
+    while ((opt = getopt(argc, argv, "hvi:d:j:")) != -1)
     {
         switch (opt)
         {
@@ -50,6 +52,9 @@ int main(int argc, char **argv)
         case 'i':
             ipPort = string(optarg);
             break;
+        case 'j':
+            processor_count = atoi(optarg);
+            break;
         default:
             std::cerr << "Illegal command line option '" << opt << "'" << endl;
             usage(argv);
@@ -61,9 +66,12 @@ int main(int argc, char **argv)
         usage(argv);
         exit(-1);
     }
+    if (processor_count == -1) {
+        std::cerr << "processor count must be specified" << endl;
+        usage(argv);
+        exit(-1);
+    }
 
-    const auto processor_count = std::thread::hardware_concurrency();
-    std::cerr << processor_count << "\n";
     verifier_service = new DafnyVerifierServiceImpl(processor_count, dafnyBinaryPath);
 
     ServerBuilder builder;
